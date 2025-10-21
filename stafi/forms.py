bf9ee7ccb9c276c4d37e7352087ee00734fd9905
aniledaramaj <aniledaramaj@gmail.com>
2025-10-22 01:18:18 +0200

    Add files via upload

---
+++ stafi/forms.py
@@ -0,0 +1,59 @@
+from django import forms
+from .models import Profesori, Kurset, Subject, CustomUser
+from django.contrib.auth.forms import UserCreationForm
+
+
+
+class ProfesoriForm(forms.ModelForm):
+    class Meta:
+        model = Profesori
+        fields = ( 'emri', 'mbiemri', 'cel', 'email')
+        labels= {
+            'emri': 'Emri',
+            'mbiemri':'Mbiemri',
+            'cel':'Cel',
+            'email':'Email'
+        }
+
+
+wigets = {
+    'emri': forms.TextInput(attrs={'class':'form-control'}),
+    'mbiemri': forms.TextInput(attrs={'class':'form-control'}),
+    'cel': forms.NumberInput(attrs={'class':'form-control'}),
+    'email':forms.EmailInput(attrs={'class':'form-control'})
+}
+
+class KursetForm(forms.ModelForm):
+    class Meta:
+        model = Kurset
+        fields= ('leksioni', 'petagogu', 'kredite')
+        labels = {
+            'leksioni': 'Leksionet',
+            'petagogu': 'Petagogu',
+            'kredite': 'Kreditet',
+
+        }
+
+
+class SubjectFrom(forms.ModelForm):
+    class Meta:
+        model = Subject
+        fields = ('name', 'description')
+        labels = {
+            'name':'Emri',
+            'description':'Pershkrimi'
+        }
+
+
+
+class StudentRegistrationForm(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        fields = ('username', 'password1', 'password2','email')
+        labels={
+            'username':'Username',
+            'password1':'Password1',
+            'password2':'Password2',
+            'email':'Email'
+        }
+
